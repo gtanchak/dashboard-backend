03a777147b660835b37b81df32021dbf49f09a6a
ghanshyamtanchak <ghtanchak77@gmail.com>
2022-07-19 15:14:33 +0530

    create express server

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 const express = require('express')
-
+const path = require('path')
 const PORT = process.env.PORT || 3500
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', {root: __dirname})
+app.get('^/$|/index(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
 })
+
+app.get('/new-page(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'new-page.html'))
+})
+
+const one = (req, res, next) => {
+    console.log("one")
+    next()
+}
+
+const two = (req, res, next) => {
+    console.log("two")
+    next()
+}
+
+const three = (req, res, next) => {
+    console.log("three")
+    res.send('Finished')
+}
+
+app.get('/chain(.html)?', [one, two, three])
+
 
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`))
