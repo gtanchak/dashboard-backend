3947518a9069c9529dfa5fa6192aed67e4fc73e9
ghanshyamtanchak <ghtanchak77@gmail.com>
2022-07-20 19:50:49 +0530

    add error middleware

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path')
 const PORT = process.env.PORT || 3500
 const {logger} = require('./middleware/logEvents')
 const cors = require('cors')
+const errorHandler = require('./middleware/errorHandler')
  
 const app = express()
 
@@ -18,7 +19,7 @@ app.use(express.static(path.join(__dirname, '/public')))
 app.use(logger)
 
 // cross origin resource sharing
-const whitList = ["https://www.google.com/s", "http://localhost:3500/"]
+const whitList = ["https://www.google.com/", "http://localhost:3500/"]
 
 const corsOptions = {
     origin: (origin, callback) => {
@@ -28,10 +29,13 @@ const corsOptions = {
         else {
             callback(new Error("Not allowed by CORS"))
         }
-    }
+    },
+    optionsSuccessStatus: 200
 }
 
 app.use(cors(corsOptions))
+
+app.use(errorHandler)
 
 app.get('^/$|/index(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'))
@@ -39,6 +43,10 @@ app.get('^/$|/index(.html)?', (req, res) => {
 
 app.get('/new-page(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'new-page.html'))
+})
+
+app.get('/*', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`))
