fea78d51cc73747496d4ce399fef91e76cc44386
ghanshyamtanchak <ghtanchak77@gmail.com>
2022-07-20 09:27:52 +0530

    add cors packege

--- server.js
+++ server.js
@@ -1,8 +1,37 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 3500
-
+const {logger} = require('./middleware/logEvents')
+const cors = require('cors')
+ 
 const app = express()
+
+// built in middleware for urlencoded
+app.use(express.urlencoded({extended: false}))
+
+// built in middleware for json
+app.use(express.json())
+
+// for static files
+app.use(express.static(path.join(__dirname, '/public')))
+
+app.use(logger)
+
+// cross origin resource sharing
+const whitList = ["https://www.google.com/s", "http://localhost:3500/"]
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        if(!whitList.includes(origin)) {
+            callback(null, true)
+        }
+        else {
+            callback(new Error("Not allowed by CORS"))
+        }
+    }
+}
+
+app.use(cors(corsOptions))
 
 app.get('^/$|/index(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'))
@@ -11,23 +40,5 @@ app.get('^/$|/index(.html)?', (req, res) => {
 app.get('/new-page(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'new-page.html'))
 })
-
-const one = (req, res, next) => {
-    console.log("one")
-    next()
-}
-
-const two = (req, res, next) => {
-    console.log("two")
-    next()
-}
-
-const three = (req, res, next) => {
-    console.log("three")
-    res.send('Finished')
-}
-
-app.get('/chain(.html)?', [one, two, three])
-
 
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`))
