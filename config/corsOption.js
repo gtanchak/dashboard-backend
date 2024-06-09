36def2a6352d52c84fa214dbbd495ff79474ace0
gtanchak <ghtanchak77@gmail.com>
2024-06-09 22:08:24 +0530

    feat: initialise the typescript

--- config/corsOption.js
+++ config/corsOption.js
@@ -1,14 +1,13 @@
 const whitelist = ["http://localhost:3500"];
-
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not Allowed by CORS"));
-    }
-  },
-  optionsSuccessStatus: 200,
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        }
+        else {
+            callback(new Error("Not Allowed by CORS"));
+        }
+    },
+    optionsSuccessStatus: 200,
 };
-
-module.exports = corsOptions;
+export default corsOptions;
