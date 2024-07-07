c813d4d0cf3186751b3cb0c4027be2834feaf7a7
gtanchak <ghtanchak77@gmail.com>
2024-07-07 18:40:25 +0530

    working on backend server

--- routes/auth.ts
+++ routes/auth.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 
 const router = express.Router();
 
-router.post("/", (req: Request, res: Response) => {});
+router.post("/", (req: Request, res: Response) => {
+  console.log(req, res);
+});
 
 export default router;
